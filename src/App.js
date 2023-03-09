d1a94edbfd8e14e5fa0e0b63a85936a35aa6955b
debora <debora.sesv@gmail.com>
2023-03-09 16:36:01 -0300

    criação de um formulario no app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [formValues, setFormValues] = useState({});
+
+  const handleInputChange = (event) => {
+    const {name, value} = event.target;
+    setFormValues({...formValues, [name]: value});
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+
+    console.log('***hendleSubmit', data)
+
+  }
+
+  console.log('***formValues', formValues);
+  
+    return ( 
+
+    <form onSubmit={handleSubmit}>
+    <div>
+      <h1>Formulário</h1>
     </div>
+    <input type="text" name="name" placeholder="name" onChange={handleInputChange} value={formValues.name || ''}/>
+
+    <input type="email" name="email" placeholder="seuemail@dominio.com" onChange={handleInputChange} value={formValues.email || ''} />
+  
+    
+    <input type="tel" name="telefone" placeholder="(00) 0 0000-0000" onChange={handleInputChange} value={formValues.telefone || ''}/>
+      
+      <div className='radios'>
+          <label>
+
+          <input type="radio" value="whatsapp" name="contact" onChange={handleInputChange} checked={formValues.contact ==="whatsapp"}/> Whatsapp
+          
+          </label>
+          
+          <label>
+
+          <input type="radio" value="email" name="contact" onChange={handleInputChange} checked={formValues.contact ==="email"}/> Email
+
+          </label>
+        
+      </div>
+
+        <textarea name="message" onChange={handleInputChange} value={formValues.message || ''}></textarea>
+
+    <button type='submit'>Enviar</button>
+    
+    </form>
   );
+  
 }
 
 export default App;
